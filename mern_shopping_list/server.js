bf524a846650fdbe4ae3259c838770b83b6854a4
Jon McKay <jmckay0411@gmail.com>
2019-01-07 20:50:04 -0700

    Use router.get to retrieve database items

--- mern_shopping_list/server.js
+++ mern_shopping_list/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 // Bodyparser middleware
@@ -14,6 +16,8 @@ const db = require('./config/keys').mongoURI;
 mongoose.connect(db, {useNewUrlParser:true})
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
+
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 
