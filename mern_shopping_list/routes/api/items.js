89f811712561e20ac9e3093eaa2aafec39be2d3f
Jon McKay <jmckay0411@gmail.com>
2019-01-08 20:43:33 -0700

    Add post function to server in order to add items to the database

--- mern_shopping_list/routes/api/items.js
+++ mern_shopping_list/routes/api/items.js
@@ -14,3 +14,14 @@ router.get('/', (req, res) => {
         .sort({ date: -1})
         .then(items => res.json(items))
 });
+
+// @route   POST api/items
+// @desc    Create a post
+// @access  Public
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    newItem.save().then(item => res.json(item));
+});
