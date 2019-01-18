a2c12fb0c061c61ad166bd6c804976c069ed9277
Jon McKay <jmckay0411@gmail.com>
2019-01-18 11:23:04 -0500

    Start initial bootstrap setup for react sidebar

--- mern_shopping_list/client/src/components/AppNavbar.js
+++ mern_shopping_list/client/src/components/AppNavbar.js
@@ -11,7 +11,31 @@ import {
 } from 'reactstrap';
 
 class AppNavbar extends Component {
+    state = {
+        isOpen: false,
+    }
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
 
+    render() {
+        <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">Shopping List</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
+                    <Collapse isOpen={this.state.isOpen} nvabar>
+                        <nav className="ml-auto" navbar>
+                            <navItem>
+                                
+                            </navItem>
+                        </nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    }
 }
-
 export default AppNavbar;
