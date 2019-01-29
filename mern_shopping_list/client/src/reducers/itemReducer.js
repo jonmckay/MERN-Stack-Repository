4e1908b957687c87f9c256472e778d5dca7a10c2
Jon McKay <jmckay0411@gmail.com>
2019-01-28 19:51:56 -0700

    Begin moving static data from components to reducers

---
+++ mern_shopping_list/client/src/reducers/itemReducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+    items: [
+        { id: uuid(), name: 'Eggs' },
+        { id: uuid(), name: 'Milk' },
+        { id: uuid(), name: 'Steak' },
+        { id: uuid(), name: 'Water' }
+    ]
+}
