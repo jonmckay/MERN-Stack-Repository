942309eadf462700eb3c6aff1842b689666203ab
Jon McKay <jmckay0411@gmail.com>
2019-01-29 19:41:13 -0700

    Add a file that contains action constants

--- mern_shopping_list/client/src/reducers/itemReducer.js
+++ mern_shopping_list/client/src/reducers/itemReducer.js
@@ -1,3 +1,6 @@
+import uuid from 'uuid';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+
 const initialState = {
     items: [
         { id: uuid(), name: 'Eggs' },
@@ -5,4 +8,15 @@ const initialState = {
         { id: uuid(), name: 'Steak' },
         { id: uuid(), name: 'Water' }
     ]
+}
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case GET_ITEMS:
+            return {
+                ...state
+            }
+        default:
+            return state;
+    }
 }
