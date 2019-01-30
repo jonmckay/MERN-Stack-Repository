7b4b8eec0c295777af7c882c0d690a3ca67b1f02
Jon McKay <jmckay0411@gmail.com>
2019-01-29 19:45:32 -0700

    Send the GET_ITEMS item type to the reducer

---
+++ mern_shopping_list/client/src/actions/itemActions.js
@@ -0,0 +1,7 @@
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types';
+
+export const GET_ITEMS = () => {
+    return {
+        type: GET_ITEMS
+    };
+};
--- mern_shopping_list/client/src/components/ShoppingList.js
+++ mern_shopping_list/client/src/components/ShoppingList.js
@@ -4,14 +4,6 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import uuid from 'uuid';
 
 class ShoppingList extends Component {
-    state = {
-        items: [
-            { id: uuid(), name: 'Eggs' },
-            { id: uuid(), name: 'Milk' },
-            { id: uuid(), name: 'Steak' },
-            { id: uuid(), name: 'Water' }
-        ]
-    }
 
     render() {
         const { items } = this.state;
