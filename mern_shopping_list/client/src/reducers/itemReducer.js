d547d76292fd616ba8e31a78a6f9af45a8c42f76
Jon McKay <jmckay0411@gmail.com>
2019-02-10 15:08:43 -0700

    Remove static data from reducer and create item loading action

--- mern_shopping_list/client/src/reducers/itemReducer.js
+++ mern_shopping_list/client/src/reducers/itemReducer.js
@@ -1,14 +1,10 @@
 import uuid from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
-    items: [
-        { id: uuid(), name: 'Eggs' },
-        { id: uuid(), name: 'Milk' },
-        { id: uuid(), name: 'Steak' },
-        { id: uuid(), name: 'Candy' }
-    ]
-}
+    items: [],
+    loading: false
+};
 
 export default function(state = initialState, action) {
     switch(action.type) {
@@ -25,6 +21,11 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 items: [action.payload, ...state.items]
+            };
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                loading: true
             };
         default:
             return state;
