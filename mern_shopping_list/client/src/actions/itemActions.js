1674c5f4b891ebed18fb4d2d44691c6c7fd20329
Jon McKay <jmckay0411@gmail.com>
2019-02-10 15:18:52 -0700

    Import Axios and change GET_ITEMS action to pull from database

--- mern_shopping_list/client/src/actions/itemActions.js
+++ mern_shopping_list/client/src/actions/itemActions.js
@@ -1,9 +1,16 @@
+import axios from 'axios';
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
 
-export const getItems = () => {
-    return {
-        type: GET_ITEMS
-    };
+export const getItems = () => dispatch => {
+    dispatch(setItemsLoading());
+    axios
+        .get('/api/items')
+        .then(res =>
+            dispatch({
+                type: GET_ITEMS,
+                payload: res.data
+            })
+            )
 };
 
 export const deleteItem = (id) => {
